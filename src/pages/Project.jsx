9d4ed3a9424172c0857edf4449223c6d2dea48eb
calvindany <calvindany1102@gmail.com>
2023-08-26 16:52:45 +0700

    Finishing project page header

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,15 +8,15 @@ export default function Project() {
     <>
       <section className="project-header">
         <div>
-          <h1 className="display-1">Rozeline</h1>
+          <h1 className="display-1 highlight">Rozeline</h1>
           <h3 className="display-3">
             An website-based application that provides information about various
             types of flora present in the Flower Garden of Penajam Paser Utara
             Regency, East Kalimantan.
           </h3>
         </div>
-        <div>
-          <img src="" alt="" />
+        <div className="image-header">
+          <img src={Rozeline} alt="" />
         </div>
       </section>
     </>
