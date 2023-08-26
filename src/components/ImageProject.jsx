64adf8826712c103f1b5291738be8c2496fe39e2
calvindany <calvindany1102@gmail.com>
2023-08-26 16:19:16 +0700

    adding route to project page and its base code

--- src/components/ImageProject.jsx
+++ src/components/ImageProject.jsx
@@ -1,10 +1,21 @@
 import "../assets/css/Home.css";
 import "../assets/css/Root.css";
+
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import ImageOverlay from "./ImageOverlay";
+
 export default function ImageProject({ image, name }) {
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   navigate();
+  // });
+
   return (
     <>
-      <div className="image-project">
+      <div className="image-project" onClick={() => navigate("/project")}>
         <img src={image} alt="" />
         <ImageOverlay />
       </div>
---
+++ src/pages/Project.jsx
@@ -0,0 +1,3 @@
+export default function Project() {
+  return <h1 style={{ color: "White" }}>Masuk</h1>;
+}
